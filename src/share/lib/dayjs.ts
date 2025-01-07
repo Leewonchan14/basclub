1cfff40467b8320d30c28ff103bd15a30f473ab6
이원찬 <twoone14@tukorea.ac.kr>
2025-01-07 20:48:41 +0900

    build 오류 제거

--- src/share/lib/dayjs.ts
+++ src/share/lib/dayjs.ts
@@ -3,7 +3,6 @@ import "dayjs/locale/ko";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import _ from "lodash";
 import { z } from "zod";
 
 dayjs.extend(utc);
@@ -23,17 +22,4 @@ export const getStartEndOfMonth = (date: Dayjs) => {
   const endOfMonth = date.endOf("month").endOf("week");
 
   return { startOfMonth, endOfMonth };
-};
-
-export const getDaysInMonth = (date: Dayjs) => {
-  const { startOfMonth, endOfMonth } = getStartEndOfMonth(date);
-
-  console.log("endOfMonth: ", endOfMonth);
-
-  const cnt = endOfMonth.diff(startOfMonth, "day") + 1;
-  console.log("cnt: ", cnt);
-
-  const days = _.range(cnt).map((i) => startOfMonth.add(i, "day"));
-
-  return days;
 };
