d799525948d96b07fa04ddb1f35e983295a4d567
이원찬 <twoone14@tukorea.ac.kr>
2025-01-14 15:26:45 +0900

    fetchscoreby event refetch시 scoreMap 무효화로 같이 가져오게 설정

--- src/feature/score/hooks/useFetchScoresByEvents.ts
+++ src/feature/score/hooks/useFetchScoresByEvents.ts
@@ -2,21 +2,34 @@
 
 import { useFetchSelectedEvents } from "@/feature/events/hooks/useFetchEventsByDate";
 import { scoreQueryApi } from "@/feature/score/score-query";
+import { getQueryClient } from "@/share/lib/tasntack-query/get-query-client";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 export const useFetchLastScoresByEvents = () => {
   const { events } = useFetchSelectedEvents();
   const { data, isFetching, refetch, fetchNextPage, isLoading } =
     useInfiniteQuery(scoreQueryApi.findScoreByEvents(events?.id ?? ""));
+
   const isNoScore = data?.pages?.[0]?.length === 0;
   const hasNext = data?.pages?.at(-1)?.length === 5;
+
+  const invalid = () => {
+    getQueryClient().invalidateQueries({
+      queryKey: [
+        ...scoreQueryApi.findAvgScoresByEvents(events?.id ?? "").queryKey,
+      ],
+    });
+  };
 
   return {
     data,
     scores: data?.pages.flat() ?? [],
     isLoading,
     isFetching,
-    refetch,
+    refetch: () => {
+      invalid();
+      refetch();
+    },
     fetchNextPage,
     isNoScore,
     hasNext,
