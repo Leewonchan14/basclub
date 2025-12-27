5e8c682dfd0e7812e43b710dbda89a3c01a8a94c
Leewonchan <twoone14@tukorea.ac.kr>
2025-12-27 12:13:51 +0900

    fix: [api] add force-dynamic to recent events route

--- src/app/api/events/recent/route.ts
+++ src/app/api/events/recent/route.ts
@@ -2,6 +2,8 @@ import { EventsService } from "@/feature/events/events.service";
 import { getService } from "@/share/lib/typeorm/DIContainer";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async () => {
   try {
     const eventService = getService(EventsService);
